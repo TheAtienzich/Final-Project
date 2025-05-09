a705f2c66140ea9c5272f3b223a2e335aa1f09a9
jorjete <jorgereyes82_@hotmail.com>
2025-05-09 17:10:02 +0200

    Commit para actualizar la clave del admin de nuevo

--- Admin.java
+++ Admin.java
@@ -5,7 +5,7 @@ public class Admin extends Usuario {
 
     public Admin(String email, String contrasenya, String claveEmpresa, String nombre){
         super(email, contrasenya, nombre);
-        this.claveEmpresa = "0000000F";
+        this.claveEmpresa = "000000F";
     }
 
     @Override
