b97ae0df29eb4d9113d318b33da2f58b5099cccb
Salet <salet.arlet@gmail.com>
2025-05-16 20:17:42 +0200

    Aumento de funciones de mostrar clientes y mostrar ordenes
    nuevo metodo cliente tostring

--- Admin.java
+++ Admin.java
@@ -17,12 +17,27 @@ public class Admin extends Usuario {
     public String ToString(){
         return super.ToString() + " Clave Empresa: " + claveEmpresa;
     }
-    // Agregar velas, y borrar velas
-    //
-    @Override
-    public String VerAdmin()
+    public static void showClients(List<Cliente>clientes)
     {
-                return super.ToString();
+        System.out.println("List of clients: ");
+        for(Cliente c : clientes)
+        {
+            System.out.println(c);
+            System.out.println("------------");
+        }
     }
-
+    public static void showOrders(List<Cliente> clientes)
+    {
+        for(Cliente c : clientes)
+        {
+            System.out.println("Client: " + c.getName());
+            System.out.println("Email: " + c.getEmail());
+            System.out.println("Pedidos: ");
+            for (Order o: c.getOrdersClient())
+            {
+                System.out.println(o);
+            }
+            System.out.println("==========");
+        }
+    }
 }
