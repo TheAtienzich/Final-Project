04b4512a1fe2ea3007cff9903fb557525b32d525
Salet <salet.arlet@gmail.com>
2025-05-20 17:12:53 +0200

    funcion de añadir velas hecha

--- ProductManagement.java
+++ ProductManagement.java
@@ -1,12 +1,43 @@
 import java.util.*;
 public class ProductManagement {
-    // añadir velas
-    // descontar velas con compra
+    static Scanner sc = new Scanner(System.in);
     private List<Candle> candles = new ArrayList<>();
 
-    public void addCandlesAdmin(Candle c, int count){
-        c.AnyadirCantidad(count);
-        System.out.println("Stock increased correctly ");
-    }
+    // Y Tambien se puede implementar ver stock
+    public void adminStockMenu(Candle c){
+        int option;
+        String codeCandle;
+        Candle candle = null;
+        int count;
 
+        do {
+            System.out.println(" Stock Candles: ");
+            System.out.println("0. EXIT  ");
+            System.out.println("1. Add candles ");
+            System.out.println("2. Substract candles ");
+            option = sc.nextInt();
+
+            switch (option){
+                case 1:
+                    System.out.println("Code candle: ");
+                    codeCandle = sc.nextLine();
+                    if(verifyCandle(codeCandle)){
+                        System.out.println("Tell me how many candles you want to add: ");
+                        count = sc.nextInt();
+                        candle.AnyadirCantidad(count);
+                        System.out.println("Candle successfully");
+                    } else {
+                        System.out.println("Incorrect code candle");
+                    }
+            }
+        } while(option != 0) ;
+    }
+    public boolean verifyCandle(String codeCandle){
+        for(Candle c : candles){
+            if(c.getCandleCode().equals(codeCandle)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
