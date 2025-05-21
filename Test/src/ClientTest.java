3c0eb946e33c707b3a49aa4487f8412566a86167
Kaxi <liukaxi0413@gmail.com>
2025-05-21 19:45:49 +0200

    Test name getName() in ClientTest.java

---
+++ Test/src/ClientTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @Test
+    //Test name
+    void getName() {
+        Client c=new Client("Juan","S","123456789@gmail.com","12345");
+        assertNotNull(c.getName());
+        assertEquals("Juan", c.getName());
+    }
+
+}
